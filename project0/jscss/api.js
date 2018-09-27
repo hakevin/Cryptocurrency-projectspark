8120a856d40bd1cc29a3f47afb042e406e583b74
Kevin Ha <kevinha1694@gmail.com>
2018-09-27 14:25:40 -0400

    Async/await complete-Commit 6:-JohnN,Devin,Clay. Note: Do CSS, talk to Mitch.

--- project0/jscss/api.js
+++ project0/jscss/api.js
@@ -1,3 +1,44 @@
+/* Simpsons async style Final! */
+
+const bitcoinField = document.querySelector(".bitcoin");
+const dollarsField = document.querySelector(".dollars");
+
+
+/* async await */
+let moneyConvert = async function () {
+    const basecurrency = document.getElementById("basecurrency").value;
+    const endpoint = 'https://api.cryptonator.com/api/ticker/' + basecurrency + '-usd';
+
+    try {
+        const response = await fetch(endpoint);
+        data = (await response.json());
+        updateContent();
+    } catch (err) {
+        console.log('Something went wrong. :(');
+        console.log(err);
+    }
+}
+
+let updateContent = function () {
+    console.log(data);
+
+    const bitcoinAmount = data.ticker.price;
+    const bitcoin = bitcoinField.value;
+    const dollars = (bitcoin * bitcoinAmount).toFixed(2);
+    dollarsField.value = dollars;
+
+}
+
+document.querySelector(".convertButton").addEventListener("click", moneyConvert);
+
+
+
+
+
+
+
+
+
 /*  OG Promise  */
 
 /* 
@@ -32,67 +73,3 @@ function moneyConvert()
  */
 
 
-
-
-/* Simpsons async style Final! */
-
-const bitcoinField = document.querySelector(".bitcoin");
-const dollarsField = document.querySelector(".dollars");
-
-const basecurrency = document.getElementById("basecurrency").value;
-const endpoint = 'https://api.cryptonator.com/api/ticker/' + basecurrency + '-usd';
-
-
-/* async await */
-let moneyConvert = async function() {
-    try {
-        const response = await fetch(endpoint);
-        data = (await response.json());
-        updateContent();
-    } catch (err) {
-        console.log('Something went wrong. :(');
-        console.log(err);
-    }
-}
-
-
-let updateContent = function() {
-    console.log(data);
-
-    const bitcoinAmount = data.ticker.price;
-    const bitcoin = bitcoinField.value;
-    const dollars = (bitcoin * bitcoinAmount).toFixed(2);
-    dollarsField.value = dollars;
-
-}
-
-document.querySelector(".convertButton").addEventListener("click", moneyConvert);
-
-
-
-
-
-
-
-
-
-
-/* Promise */
-/* let getQuoteUsingPromise = function() {
-    fetch(apiUrl)
-        // This happens when data comes back in
-        .then( (response) => {
-            // Return unwrapped promise object for next chaining operation
-            return response.json();
-        })
-            // Utilize unwrapped promise as javascript object
-        .then( (data) => {
-            state = data[0];
-            updateContent();
-        })
-        // This happens if there is a problem with the request
-        .catch( (error) => {
-            alert('oh no :(');
-            console.log(error);
-        });
-} */
