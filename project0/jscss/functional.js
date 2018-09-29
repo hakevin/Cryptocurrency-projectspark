b6088cf97b8a6e27e01bda44686ad06facc6f184
Kevin Ha <kevinha1694@gmail.com>
2018-09-29 18:58:14 -0400

    Final Saturday, could use more javascript functionality

---
+++ project0/jscss/functional.js
@@ -0,0 +1,49 @@
+function bitFunction() {
+    document.getElementById("bitbutton").innerHTML = "The first blockchain created by Satoshi Nakamoto in 2009. Genesis block read -The Times 03/Jan/2009 Chancellor on brink of second bailout for banks!";
+    
+}
+
+function ethFunction() {
+    document.getElementById("ethbutton").innerHTML = "Introduced in  2015 as a platform to release ICOs and dApps. Proposed by Vitalik Buterin and hardforked in 2016 into Ethereum Classic.";
+}
+
+function xrpFunction() {
+    document.getElementById("xrpbutton").innerHTML = "Ripple is a real-time gross settlement system, currency exchange and remittance network created by Ripple Labs Inc. Seeks to replace Swift.";
+}
+
+function adaFunction() {
+    document.getElementById("adabutton").innerHTML = "3rd generation, peer reviewed and provably secure.";
+}
+
+function mkrFunction() {
+    document.getElementById("mkrbutton").innerHTML = "An Ethereum based protocol that provides an auditable stable token pegged to the USD.";
+}
+
+function dogeFunction() {
+document.getElementById("dogebutton").innerHTML = "A Meme, joke coin based on prrof of work. Ranked 20.";
+}
+
+function usdFunction() {
+    document.getElementById("usdbutton").innerHTML = "The king of all currencies.!";
+    
+}
+
+function eurFunction() {
+    document.getElementById("eurbutton").innerHTML = "Currently, 19 of 28 member states use the euro; this group of states is known as the eurozone or euro area. ";
+}
+
+function vndFunction() {
+    document.getElementById("vndbutton").innerHTML = "One of the highest inflating currencies in Asia. Often traded for USDs.";
+}
+
+function cnyFunction() {
+    document.getElementById("cnybutton").innerHTML = "Based on global trade of 13 countries.";
+}
+
+function rubFunction() {
+    document.getElementById("rubbutton").innerHTML = "Faced a financial crisis between 2014-2017 concerning the government's manipulation of the inflation and interest rates.";
+}
+
+function vefFunction() {
+document.getElementById("vefbutton").innerHTML = "Meme, joke currency. Inflation sent the currency into worthlessness. Maduro attemps to use a cryptocurrency called the Petro.";
+}
